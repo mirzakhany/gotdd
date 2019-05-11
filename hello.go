ec73af151d4c4ab0b497f1ab75adaa86ad093778
mohsen mirzakhani <mohsen.mirzakhani@snapptrip.com>
2019-05-11 16:34:57 +0430

    add comments, fix empty message

--- hello.go
+++ hello.go
@@ -2,15 +2,18 @@ package main
 
 import "fmt"
 
+// HelloPrefix is hello prefix
 const HelloPrefix = "Hello, "
 
-
+// Hello return a hello message
 func Hello(name string) string {
+	if name == "" {
+		name = "World"
+	}
 	return HelloPrefix + name
 }
 
 func main() {
 
 	fmt.Println(Hello("mohsen"))
-
 }
--- hello_test.go
+++ hello_test.go
@@ -2,12 +2,29 @@ package main
 
 import "testing"
 
-func TestHello(t *testing.T){
+func TestHello(t *testing.T) {
 
-	got := Hello("Mohsen")
-	want := HelloPrefix + "Mohsen"
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
 
-	if got != want {
-		t.Errorf("got '%s' want '%s'", got, want)
+		if got != want {
+			t.Errorf("got '%s' want '%s'", got, want)
+		}
 	}
+
+	t.Run("say hello to mohsen", func(t *testing.T) {
+
+		got := Hello("Mohsen")
+		want := HelloPrefix + "Mohsen"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say hello world when empty name provided", func(t *testing.T) {
+
+		got := Hello("")
+		want := HelloPrefix + "World"
+
+		assertCorrectMessage(t, got, want)
+	})
 }

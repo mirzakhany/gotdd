247c9c415bbaa0598b76c70dee67e52516cc4315
mohsen mirzakhani <mohsen.mirzakhani@snapptrip.com>
2019-05-15 17:03:02 +0430

    add sumAllTail and test

--- arrayOps/arrayops.go
+++ arrayOps/arrayops.go
@@ -19,3 +19,13 @@ func SumAll(numbersToSum ...[]int) []int {
 	}
 	return sums
 }
+
+// SumAllTail ...
+func SumAllTail(numberToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numberToSum {
+		tail := numbers[1:]
+		sums = append(sums, ArraySum(tail))
+	}
+	return sums
+}

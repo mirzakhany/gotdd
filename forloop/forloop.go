cb61d315390d8a0d424faf6d1054772ed5ec9cfb
mohsen mirzakhani <mohsen.mirzakhani@snapptrip.com>
2019-05-15 15:39:35 +0430

    for loop test with benchmark

--- forloop/forloop.go
+++ forloop/forloop.go
@@ -1,1 +1,12 @@
 package forloop
+
+// Repeate repeate a char n time
+func Repeate(char string, count int) string {
+
+	var repeated string
+	for i := 0; i < count; i++ {
+		repeated += char
+	}
+
+	return repeated
+}
--- forloop/forloop_test.go
+++ forloop/forloop_test.go
@@ -1,1 +1,19 @@
 package forloop
+
+import "testing"
+
+func TestRepeat(t *testing.T) {
+
+	expected := "aaaaa"
+	repeated := Repeate("a", 5)
+
+	if repeated != expected {
+		t.Errorf("expected '%s' but got '%s'", expected, repeated)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeate("a", 5)
+	}
+}
